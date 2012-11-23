4a7c456090aa71ea735f6c70adc03fb4eaae6846
Will <w.pond@sms.ed.ac.uk>
2012-11-23 18:38:22 +0000

    initial commit

---
+++ main.c
@@ -0,0 +1,129 @@
+
+#include "efm32.h"
+
+#include "efm32_chip.h"
+#include "efm32_rtc.h"
+#include "efm32_gpio.h"
+#include "efm32_cmu.h"
+#include "efm32_timer.h"
+#include "efm32_int.h"
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "scheduler.h"
+#include "tasks.h"
+#include "system.h"
+#include "led.h"
+
+void initClocks();
+void enableTimers();
+void enableInterrupts();
+
+int main()
+{
+	
+	// Chip errata
+	CHIP_Init();
+	
+	// ensure core frequency has been updated
+	SystemCoreClockUpdate();
+	
+	// start clocks
+	initClocks();
+	
+	// init timer service
+	TIMER_InitCallbacks();
+	
+	// init LEDs
+	LED_Init();
+	
+	// init trace
+	TRACE_Init();
+	
+	// init scheduler
+	SCHEDULER_Init();
+	
+	// enable interrupts
+	enableInterrupts();
+	
+	// init tasks
+	SCHEDULER_TaskInit(&radio_init_task, radio_init_task_entrypoint);
+	SCHEDULER_TaskInit(&display_init_task, display_init_task_entrypoint);
+	
+	// run
+	SCHEDULER_Run();
+	
+}
+
+void enableInterrupts()
+{
+	
+	NVIC_EnableIRQ(SysTick_IRQn);
+	NVIC_EnableIRQ(PendSV_IRQn);
+	NVIC_SetPriority(PendSV_IRQn, 7);
+	NVIC_SetPriority(SysTick_IRQn, 7);
+	
+	NVIC_EnableIRQ(USART2_TX_IRQn);
+	NVIC_EnableIRQ(USART2_RX_IRQn);
+	
+	NVIC_EnableIRQ(GPIO_EVEN_IRQn);
+	
+	NVIC_EnableIRQ(TIMER0_IRQn);
+	NVIC_EnableIRQ(TIMER1_IRQn);
+	
+	NVIC_EnableIRQ(UART1_TX_IRQn);
+	NVIC_EnableIRQ(UART1_RX_IRQn);
+	NVIC_SetPriority(UART1_TX_IRQn, 6);
+	NVIC_SetPriority(UART1_RX_IRQn, 6);
+	
+}
+
+void initClocks()
+{
+	
+	/* Starting LFXO and waiting until it is stable */
+	CMU_OscillatorEnable(cmuOsc_LFXO, true, true);
+
+	// starting HFXO, wait till stable
+	CMU_OscillatorEnable(cmuOsc_HFXO, true, true);
+
+	// route HFXO to CPU
+	CMU_ClockSelectSet(cmuClock_HF, cmuSelect_HFXO);
+
+	/* Routing the LFXO clock to the RTC */
+	CMU_ClockSelectSet(cmuClock_LFA, cmuSelect_LFXO);
+	CMU_ClockSelectSet(cmuClock_LFB, cmuSelect_LFXO);
+
+	// disabling the RCs
+	CMU_ClockEnable(cmuSelect_HFRCO, false);
+	CMU_ClockEnable(cmuSelect_LFRCO, false);
+
+	/* Enabling clock to the interface of the low energy modules */
+	CMU_ClockEnable(cmuClock_CORE, true);
+	CMU_ClockEnable(cmuClock_CORELE, true);
+
+	// enable clock to hf perfs
+	CMU_ClockEnable(cmuClock_HFPER, true);
+
+	// enable clock to GPIO
+	CMU_ClockEnable(cmuClock_GPIO, true);
+
+	// enable clock to RTC
+	CMU_ClockEnable(cmuClock_RTC, true);
+	RTC_Enable(true);
+
+	// enable radio usart
+	CMU_ClockEnable(cmuClock_USART2, true);
+	
+	// enable pc serial
+	CMU_ClockEnable(cmuClock_UART1, true);
+	
+	// enable timers
+	CMU_ClockEnable(cmuClock_TIMER0, true);
+	CMU_ClockEnable(cmuClock_TIMER1, true);
+	
+	// i2c
+	CMU_ClockEnable(cmuClock_I2C0, true);
+	
+}
